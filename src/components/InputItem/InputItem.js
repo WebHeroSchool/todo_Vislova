d8ef3d5d144aba74a5533e3813e87ed295142bee
Ksenia Vislova <kseniavislova@gmail.com>
2021-06-23 18:28:46 +0300

    Lesson25

--- src/components/InputItem/InputItem.js
+++ src/components/InputItem/InputItem.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import TextField from '@material-ui/core/TextField';
 
 const InputItem = () => (
   <div>
-    <input type="text" placeholder="введи свою важную задачу"></input>
+    <TextField
+      id="outlined-full-width"
+      label="New task"
+      style={{ margin: 8 }}
+      placeholder="Добавь новую задачу"
+      fullWidth
+      margin="normal"
+      InputLabelProps={{
+        shrink: true,
+      }}
+      variant="outlined"
+    />
   </div>
 );
 
