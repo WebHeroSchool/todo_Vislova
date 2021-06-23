4d95605096c6666d4da5802b7e089dc63341273b
Ksenia Vislova <kseniavislova@gmail.com>
2021-06-23 14:26:52 +0300

    Урок 17

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const newElement = (
+const ItemList = () => (
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+    <li>4</li>
+    <li>5</li>
+  </ul>
+);
+
+const App = () => (
   <div>
-    Новый элемент для нового приложения
-    <a href="#">Ссылочка, которая пока не работает</a>
+    <h1>Заголовок для нашего приложения</h1>
+    <ItemList />
   </div>
 );
 
-export default newElement;
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import newElement from './App';
+import App from './App';
 
-ReactDOM.render(newElement, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
