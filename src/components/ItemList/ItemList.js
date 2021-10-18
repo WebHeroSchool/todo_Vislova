a74c5bda87ba9c396a97edd66f5ad5095a502d22
Ksenia Vislova <74788256+KseniaVislova@users.noreply.github.com>
2021-10-18 13:25:13 +0300

    tried to add drag and drop
    Need to commit change because i can lose accept to my laptop

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useCallback } from 'react';
 import Item from './Item/Item';
 import styles from './ItemList.module.css';
 import PropTypes from 'prop-types';
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider} from 'react-dnd';
+import update from 'immutability-helper';
 
-const ItemList = ({ items, onClickDone, onClickDelete }) => (
-  <ul className={styles.list}>
-    {items.map((item) => (
-      <li key={item.id}>
-        <Item
-          value={item.value}
-          isDone={item.isDone}
-          id={item.id}
-          onClickDone={onClickDone}
-          onClickDelete={onClickDelete}
-        />
-      </li>
-    ))}
-  </ul>
-);
+const ItemList = (props) => {
+  const [items, setItems] = useState(props);
+  const {onClickDone, onClickDelete} = props;
+
+  const moveItem = useCallback((dragIndex, hoverIndex) => {
+    const dragItem = items[dragIndex];
+    setItems(update(items, {
+        $splice: [
+            [dragIndex, 1],
+            [hoverIndex, 0, dragItem],
+        ],
+    }));
+}, [items]);
+
+console.log(items)
+
+  return <div>
+    <DndProvider backend={HTML5Backend}>
+    <ul className={styles.list}>
+      {items.map((item) => (
+        <li key={item.id}>
+          <Item
+            value={item.value}
+            isDone={item.isDone}
+            id={item.id}
+            onClickDone={onClickDone}
+            onClickDelete={onClickDelete}
+            moveItem={moveItem}
+          />
+        </li>
+      ))}
+    </ul>
+		</DndProvider>
+  </div>
+}
 
 ItemList.propTypes = {
   items: PropTypes.array.isRequired,
